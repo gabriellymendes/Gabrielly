9b2b109eef2c17deecd9b11be6ae48bab4c6b5fc
gabrielly mendes pacheco <135851177+gabriellymendes@users.noreply.github.com>
2025-09-26 11:44:35 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Previne o envio do formulário para demonstrar a funcionalidade
+
+  alert('Formulário enviado com sucesso!');
+});
